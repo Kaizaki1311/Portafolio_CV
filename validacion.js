17e16aa70de8aa0ff69749d2fe3fdf3d0bbe3eef
Kaizaki1311 <Edgar.CP1311@gmail.com>
2024-07-08 20:59:44 -0600

    Ver 1

---
+++ validacion.js
@@ -0,0 +1,37 @@
+function validarDatos() {
+    window.event.preventDefault()
+
+    if (document.form.nombre.value == "") {
+        alert("Campo nombre es obligatorio")
+        document.form.nombre.focus()
+
+    } else if (document.form.email.value == "") {
+        alert("Campo e-mail es obligatorio")
+        document.form.email.focus()
+
+    } else if (document.form.asunto.value == "") {
+        alert("Campo Asunto es obligatorio")
+        document.form.asunto.focus()
+
+    } else if (document.form.mensaje.value == "" || document.form.mensaje.value.length <= 50) {
+        alert("Campo Mensaje es obligatorio y debe contener máximo 50 carateres")
+        document.form.mensaje.focus()
+
+    } else if (document.form.email.value.indexOf('@') == -1 ||
+        document.form.email.value.indexOf('.') == -1) {
+        alert("e-mail inválido")
+    }
+
+}
+document.querySelector('form').addEventListener('submit', validarDatos)
+
+const fulImgBox = document.getElementById("fulImgBox"),
+    fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference) {
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg() {
+    fulImgBox.style.display = "none";
+}
